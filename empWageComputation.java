b1d49afd9f34a0668687d3628d2047fddfdca651
unknown <miyashaheen1995@gmail.com>
2020-11-11 00:19:24 +0530

    Refactor : Changed if else to switch statements

--- empWageComputation.java
+++ empWageComputation.java
@@ -1,24 +1,25 @@
 package day19Assignment;
 
 public class empWageComputation {
+	public static final int IS_PART_TIME=1;
+	public static final int IS_FULL_TIME=2;
+	public static final int EMP_RATE_PER_HOUR=20;
+	
 	public static void main(String []args) {
-
 		System.out.println("Welcome to Employee Wage Computation Program !");
-		//Constants
-		int IS_PART_TIME=1;
-		int IS_FULL_TIME=2;
-		int EMP_RATE_PER_HOUR=20;
 		//variables
 		int empHrs=0;
 		int empWage=0;
 		//use random function to generate 0,1 and 2
-		double empCheck=Math.floor(Math.random()*10)%3;
-		if (empCheck == IS_PART_TIME) 
-			empHrs=4;
-		else if (empCheck == IS_FULL_TIME)
-			empHrs=8;
-		else
-			empHrs=0;
+		int empCheck=(int) Math.floor(Math.random()*10)%3;
+		switch (empCheck) {
+		case IS_PART_TIME:empHrs=4;
+						   break;
+		case IS_FULL_TIME:empHrs=8;
+						   break;
+		default: empHrs=0;	
+				 break;
+		}
 		empWage=empHrs*EMP_RATE_PER_HOUR;
 		System.out.println("Employee Wage = "+empWage);
 	}
