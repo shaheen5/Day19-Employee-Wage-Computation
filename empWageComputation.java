15a5f66fd286d015724f0d3512bcc735330d9aa1
unknown <miyashaheen1995@gmail.com>
2020-11-10 23:31:38 +0530

    Added : Code to check employee presence using random method

--- empWageComputation.java
+++ empWageComputation.java
@@ -4,6 +4,14 @@ public class empWageComputation {
 	public static void main(String []args) {
 
 		System.out.println("Welcome to Employee Wage Computation Program !");
+		//Constants
+		int IS_FULL_TIME=1;
+		//use random function to generate 0 and 1
+		double empCheck=Math.floor(Math.random()*10)%2;
+		if (empCheck == IS_FULL_TIME) 
+			System.out.println("Employee is Present");
+		else
+			System.out.println("Employee is absent");
 	}
 
 }
